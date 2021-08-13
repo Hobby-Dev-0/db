bbc0bf55633eaf01f15927bdc7fd4b4758582924
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 11:37:55 +0530

    iampro

--- Extre/utilss.py
+++ Extre/utilss.py
@@ -34,7 +34,7 @@ def load_plugins(plugin_name):
         )
         from .misc._decorators import extremepiro_cmd
         from .misc._wrappers import eod, eor
-
+        from .utils import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
         path = Path(f"plugins/{plugin_name}.py")
         name = "plugins.{}".format(plugin_name)
         spec = util.spec_from_file_location(name, path)
