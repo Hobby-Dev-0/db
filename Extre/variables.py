03df072b3317ae853002ad65642298b1acbe7699
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 11:52:57 +0530

    k

--- Extre/variables.py
+++ Extre/variables.py
@@ -2,7 +2,7 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from Extre.heroku import Var as Config
+    from Extre.config import Var as Config
     from .var import Var
 else:
     from local_config import Development as Config
