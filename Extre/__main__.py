bba96d380ce8936956e8529d4da9723d3a049a2a
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 12:56:14 +0530

    k

--- Extre/__main__.py
+++ Extre/__main__.py
@@ -16,91 +16,6 @@ import asyncio
 import telethon.utils
 
 from .var import Var
-
-async def autobot():
-    from .var import Var
-    await extremepro_bot.start()
-    if Var.BOT_TOKEN:
-        ExtremedB.set("BOT_TOKEN", str(Var.BOT_TOKEN))
-        return
-    if ExtremedB.get("BOT_TOKEN"):
-        return
-    LOGS.info("MAKING A TELEGRAM BOT FOR YOU AT @BotFather , Please Kindly Wait")
-    who = await extremepro_bot.get_me()
-    name = who.first_name + "'s Assistant Bot"
-    if who.username:
-        username = who.username + "_bot"
-    else:
-        username = "extremepro_" + (str(who.id))[5:] + "_bot"
-    bf = "Botfather"
-    await extremepro_bot(UnblockRequest(bf))
-    await extremepro_bot.send_message(bf, "/cancel")
-    await asyncio.sleep(1)
-    await extremepro_bot.send_message(bf, "/start")
-    await asyncio.sleep(1)
-    await extremepro_bot.send_message(bf, "/newbot")
-    await asyncio.sleep(1)
-    isdone = (await extremepro_bot.get_messages(bf, limit=1))[0].text
-    if isdone.startswith("That I cannot do."):
-        LOGS.info(
-            "Please make a Bot from @BotFather and add it's token in BOT_TOKEN, as an env var and restart me."
-        )
-        exit(1)
-    await extremepro_bot.send_message(bf, name)
-    await asyncio.sleep(1)
-    isdone = (await extremepro_bot.get_messages(bf, limit=1))[0].text
-    if not isdone.startswith("Good."):
-        await extremepro_bot.send_message(bf, "My Assistant Bot")
-        await asyncio.sleep(1)
-        isdone = (await extremepro_bot.get_messages(bf, limit=1))[0].text
-        if not isdone.startswith("Good."):
-            LOGS.info(
-                "Please make a Bot from @BotFather and add it's token in BOT_TOKEN, as an env var and restart me."
-            )
-            exit(1)
-    await extremepro_bot.send_message(bf, username)
-    await asyncio.sleep(1)
-    isdone = (await extremepro_bot.get_messages(bf, limit=1))[0].text
-    await extremepro_bot.send_read_acknowledge("botfather")
-    if isdone.startswith("Sorry,"):
-        ran = randint(1, 100)
-        username = "extremepro_" + (str(who.id))[6:] + str(ran) + "_bot"
-        await extremepro_bot.send_message(bf, username)
-        await asyncio.sleep(1)
-        nowdone = (await extremepro_bot.get_messages(bf, limit=1))[0].text
-        if nowdone.startswith("Done!"):
-            token = nowdone.split("`")[1]
-            ExtremedB.set("BOT_TOKEN", token)
-            await extremepro_bot.send_message(bf, "/setinline")
-            await asyncio.sleep(1)
-            await extremepro_bot.send_message(bf, f"@{username}")
-            await asyncio.sleep(1)
-            await extremepro_bot.send_message(bf, "Search")
-            LOGS.info(f"DONE YOUR TELEGRAM BOT IS CREATED SUCCESSFULLY @{username}")
-        else:
-            LOGS.info(
-                f"Please Delete Some Of your Telegram bots at @Botfather or Set Var BOT_TOKEN with token of a bot"
-            )
-            exit(1)
-    elif isdone.startswith("Done!"):
-        token = isdone.split("`")[1]
-        ExtremedB.set("BOT_TOKEN", token)
-        await extremepro_bot.send_message(bf, "/setinline")
-        await asyncio.sleep(1)
-        await extremepro_bot.send_message(bf, f"@{username}")
-        await asyncio.sleep(1)
-        await extremepro_bot.send_message(bf, "Search")
-        LOGS.info(f"DONE YOUR TELEGRAM BOT IS CREATED SUCCESSFULLY @{username}")
-    else:
-        LOGS.info(
-            f"Please Delete Some Of your Telegram bots at @Botfather or Set Var BOT_TOKEN with token of a bot"
-        )
-        exit(1)
-
-
-if not ExtremedB.get("BTOKEN"):
-    extremepro_bot.loop.run_until_complete(autobot())
-
                    
 async def add_bot(bot_token):
     await client.start(bot_token)
@@ -152,17 +67,19 @@ for name in files:
         path1 = Path(f.name)
         shortname = path1.stem
         start_assistant(shortname.replace(".py", ""))
-# ForTheGreatrerGood
       
-import glob
 
-path = 'plugins/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+# for userbot
+files = sorted(os.listdir("plugins"))
+for plugin_name in files:
+    try:
+        if plugin_name.endswith(".py"):
+            load_plugins(plugin_name[:-3])
+            if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                LOGS.info(f"ExtremeProUserbot - Official -  Installed - {plugin_name}")
+    except Exception:
+        LOGS.info(f"ExtremeProUserbot - Official - ERROR - {plugin_name}")
+        LOGS.info(str(traceback.print_exc()))
 
 
 
