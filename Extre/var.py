1cb60ce53712fed875bfa124809731bef58c5c00
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 10:54:57 +0530

    kk

--- Extre/var.py
+++ Extre/var.py
@@ -5,8 +5,9 @@ class Var:
     API_ID = os.environ.get("APP_ID")
     API_HASH = os.environ.get("API_HASH")
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = TG_BOT_TOKEN_BF_HER
     STRING_SESSION = os.environ.get("STRING_SESSION")
+    SESSION = STRING_SESSION
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     HEROKU_API_KEY = os.environ.get("HEROKU_API")
     REDIS_URI = os.environ.get("REDIS_URI")
