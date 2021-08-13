80c33c7198db74c66f50ba1b3df125826b4aa3cd
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 14:07:03 +0530

    k

--- Extre/var.py
+++ Extre/var.py
@@ -10,5 +10,6 @@ class Var:
     SESSION = STRING_SESSION
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     HEROKU_API_KEY = os.environ.get("HEROKU_API")
+    LOG_CHANNEL = os.environ.get("REDIS_URI")
     REDIS_URI = os.environ.get("REDIS_URI")
     REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
